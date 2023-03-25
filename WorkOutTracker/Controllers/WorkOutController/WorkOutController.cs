3b4ef083fa3b86745d54066eff059970858f9f70
nishant <nshntshr@gmail.com>
2023-03-25 20:20:27 +1100

    first commit

---
+++ WorkOutTracker/Controllers/WorkOutController/WorkOutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkOutTracker.Controllers.WorkOutController
+{
+    public class WorkOutController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+
+
+}
